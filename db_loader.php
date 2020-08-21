e394e6614eb194d2053e06466756a0fdb4eac077
nik <nik_mak@bk.ru>
2020-08-21 04:21:55 +0300

    update DB

--- db_loader.php
+++ db_loader.php
@@ -2,7 +2,7 @@
 
 $db = new SQLite3('test.db');
 
-$handle = fopen("output.csv", "r");
+$handle = fopen("output2.csv", "r");
 while (($data = fgetcsv($handle, 0, ":")) !== FALSE) {
     $db->exec("INSERT INTO xlsx_data VALUES('$data[0]','$data[1]','$data[2]','$data[3]','$data[4]','$data[5]','$data[6]')");
 }
